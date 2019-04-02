d442eb3482328289747a65c6f95ebe6a73b96a2e
Emil <emilpet93@gmail.com>
2019-04-02 15:12:28 +0200

    Created register page and added more styles

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/styles.css">
-    <title>Laboration 1</title>
-</head>
+<?php require 'header.php' ?>
+
 <body>
-  <main id="content">
-    <h1>Login Page</h1>
+  <main class="content">
+    <h1>Login</h1>
     <form action="index.php" method="post">
         <label for="username">Username</label>
         <input type="text" placeholder="Enter your username..." name="username">
@@ -17,9 +10,12 @@
         <input type="password" placeholder="Enter your password..." name="password"">
         <input type="submit" class="btn" value="Sign in">
     </form>
+    <span class="register">
+        Don't have an account? 
+        <a href="register.php">Register here.</a>
+    </span>
   </main>
 </body>
-</html>
 
 <?php 
 
@@ -30,29 +26,14 @@
             $inputUsername = $_POST["username"];
             $inputPassword = $_POST["password"];
 
-            // Salts
-            $salt1 = "987fed";
-            $salt2 = "654cba";
+            $url = 'logins.json';
 
-            // New user with pw
-            $username = "admin";
-            $userPw = "admin123";
-            
-            $hashedPw = password_hash($salt1 . $userPw . $salt2, PASSWORD_BCRYPT);
+            $data = file_get_contents($url);
+            $login = json_decode($data);
 
-            // Check if pw is correct
-            if (password_verify($salt1 . $userPw . $salt2, $hashedPw)) {
-                echo "You have logged in";
-            } else {
-                echo "Your password is incorrect. Try again";
+            if ($login->username == $inputUsername) {
+                echo 'Username matches';
             }
-
-            $jsonArray = array("username"=>$inputUsername, "password"=>$hashedPw);
-
-            $fp = fopen('./logins.json', 'w'); 
-            fwrite($fp, json_encode($jsonArray));
-            fclose($fp);
-
         }
 
     } catch(Exception $error) {
@@ -60,3 +41,15 @@
     }
 
 ?>
+
+<script>
+
+    var message = document.querySelector('p');
+
+    if (message.innerText === "Success! You have logged in.") {
+        message.classList.add('success');
+    } else {
+        message.classList.add('fail');
+    }
+
+</script>
